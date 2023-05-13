d45356c2210b4f098ab598d5b3834d3bfd094970
Howiemorg <morgenthalerhowie@yahoo.com>
2023-05-13 10:45:35 -0500

    created a filtered list using conditionals

--- src/components/Expenses/ManyExpenses.js
+++ src/components/Expenses/ManyExpenses.js
@@ -1,5 +1,5 @@
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
 import "./ManyExpenses.css";
 import { useState } from "react";
@@ -8,22 +8,21 @@ const ManyExpenses = (props) => {
   const [year, setYear] = useState("2022");
 
   const changeYear = (selectedYear) => {
-    setYear(selectedYear);
+    setYear((prevYear) => {
+      return selectedYear;
+    });
     console.log(year);
   };
+
+  const filteredExpenses = props.expenses.filter(
+    (expense) => expense.date.getFullYear().toString() === year
+  );
 
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={year} onChangeYear={changeYear} />
-        {props.expenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        <ExpensesList expenses={filteredExpenses} />
       </Card>
     </div>
   );
