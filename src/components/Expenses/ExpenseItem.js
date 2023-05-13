f8c36cbe9fba46fc6f10811b6debec36fa145c37
Howiemorg <morgenthalerhowie@yahoo.com>
2023-05-13 09:32:38 -0500

    created dynamic array of expenses and gave each one a key to make react optimization

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,7 +7,7 @@ const ExpenseItem = props => {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <div class="Nameexpense-item__description">
+      <div class="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
--- src/components/Expenses/ManyExpenses.js
+++ src/components/Expenses/ManyExpenses.js
@@ -4,38 +4,26 @@ import Card from "../UI/Card";
 import "./ManyExpenses.css";
 import { useState } from "react";
 
-const ManyExpenses = props => {
+const ManyExpenses = (props) => {
   const [year, setYear] = useState("2022");
 
-  const changeYear = selectedYear => {
+  const changeYear = (selectedYear) => {
     setYear(selectedYear);
     console.log(year);
   };
 
   return (
     <div>
-      <ExpensesFilter selected={year} onChangeYear={changeYear} />
       <Card className="expenses">
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        />
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        />
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        />
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        />
+        <ExpensesFilter selected={year} onChangeYear={changeYear} />
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
